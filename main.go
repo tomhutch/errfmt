cb0d4b3ae914031cdc8fff60d87f30200f3f2029
Tom Hutchinson <tom.hutchinson@deliveroo.co.uk>
2023-05-16 14:43:33 +0100

    rm unsafeptr flag overiding

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"flag"
-
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
 func main() {
-	// Don't use it: just to not crash on -unsafeptr flag from go vet
-	flag.Bool("unsafeptr", false, "")
-
 	cfg := NewDefaultConfig()
 	singlechecker.Main(NewAnalyzer(cfg))
 }
